012275133757e00aba08542851b0dc5f254b4baf
Aidan <aidlo@protonmail.com>
2021-10-31 03:32:06 +0000

    Add epochs option and more verbose console logging

--- index.js
+++ index.js
@@ -4,33 +4,48 @@ import {hashSync} from "bcrypt";
 export default options => {
 
 	options = Object.assign({
+		minSalt: Number(process.env.BCRYPT_SALT) ?? 10,
+		maxHashTime: Number(process.env.BCRYPT_MAXHASHTIME ?? 250),
+		epochs: 3,
 		quick: true,
 		quiet: false
 	}, options ?? {});
 
-	if (!options.quiet) console.log("> Performing bcrypt tests...");
+	if (!options.quiet) console.log(`> Performing bcrypt tests (Target: ${options.maxHashTime}ms)`);
 
-	process.env.BCRYPT_SALT = String(options.minSalt ?? process.env.BCRYPT_SALT ?? 10);
+	process.env.BCRYPT_SALT = String(options.minSalt);
 
 	if (!options.quiet) console.log(`  > BCRYPT_SALT set to ${process.env.BCRYPT_SALT}.`);
 
-	let currSalt = Number(process.env.BCRYPT_SALT);
-	const maxTime = Number(options.maxHashTime ?? process.env.BCRYPT_MAX_HASHTIME ?? 250);
-	const epochs = 5;
+	let currSalt = Number(options.minSalt);
+	let prevTime = test(currSalt);
+	if (prevTime > options.maxHashTime) {
+		process.emitWarning(`Minimum salt rounds (${options.minSalt}, ${prevTime}ms) exceeds target of ${options.maxHashTime}ms.`, {
+			code: "BCRYPT_TEST"
+		});
+		return fin();
+	}
 
 	while (++currSalt) {
-		const arr = [];
-		for (let i = 0; i < epochs; i++) arr.push(randomBytes(16).toString());
+		let time = 0;
+		for (let i = 0; i < options.epochs; i++) time += test(currSalt);
+		time /= options.epochs;
+		if (time > options.maxHashTime) return fin();
+		prevTime = time;
+		process.env.BCRYPT_SALT = currSalt.toString();
+		if (!options.quiet) console.log(`  > Increased BCRYPT_SALT to ${currSalt}.`);
+		if (options.quick && time > options.maxHashTime * 0.65) return fin();
+	}
+
+	function test(salt) {
+		const data = randomBytes(16).toString();
 		const start = Date.now();
-		for (let i = 0; i < epochs; i++) hashSync(arr[i], currSalt);
-		const time = (Date.now() - start) / epochs;
-		if (time > maxTime) return fin();
-		if (!options.quiet) console.log(`  > Increased BCRYPT_SALT to ${(process.env.BCRYPT_SALT = currSalt.toString())}.`);
-		if (options.quick && time > maxTime * 0.7) return fin();
+		hashSync(data, salt);
+		return Date.now() - start;
 	}
 
 	function fin() {
-		console.log();
+		if (!options.quiet) console.log(`> Done. Using ${process.env.BCRYPT_SALT} salt rounds at approx. ${Math.floor(prevTime)}ms per hash.\n`);
 		return Number(process.env.BCRYPT_SALT);
 	}
 };
